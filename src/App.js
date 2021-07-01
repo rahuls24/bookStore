5f890a934ef9413c35bf276b086bfaf0fe00cac5
Rahul Kumar <rahul@framsikt.no>
2021-07-01 21:08:04 +0530

    redesign the Navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Navbar from './components/Navbar'
-
+var navBarData = {
+  title: "Book Store",
+  buttons: ["Currently Reading", "Want to Read","Read"]
+}
 class BooksApp extends React.Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar navData = {navBarData} />
       </div>
     )
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,29 +2,38 @@ import React from 'react'
 import '../App.css'
 
 class Navbar extends React.Component {
-  render() {
+    // This method is for making id of by giving name 
+    generateId = (rawId) => {
+        let tmpId = rawId.split(' ')
+        tmpId = tmpId.map((item, index) => {
+            if (index !== 0) {
+                return item[0].toUpperCase() + item.slice(1)
+            }
+            return item.toLowerCase()
+        })
+        return "#"+tmpId.join('')
+    }
+    
+    render() {
     return (
         <nav className="navbar navbar-dark bg-primary col-sm-12 row no-gutter font-size-large">
             <div className="col-sm-6 white-color">
-                Book Store
+                {this.props.navData.title}
             </div>
             <div className="col-sm-6 row">
-                <div className="col-sm-4">
-                    <a href="#currentlyReading" className = "white-color link">
-                       Currently Reading 
-                    </a>
-                </div>
-                
-                <div className="col-sm-4">
-                    <a href="#wantToRead" className = "white-color link">
-                        Want to Read
-                    </a>
-                </div>
-                <div className="col-sm-4">
-                    <a href="#read" className = "white-color link">
-                        Read
-                    </a>
-                </div>
+                {
+                    this.props.navData.buttons.map((button) => {
+                        // We are dividing 12 col of bootstrap according to the number of item in navbar
+                        let btnClass = "col-sm-" + parseInt(12 / this.props.navData.buttons.length)
+                        return (
+                                <div className={btnClass} key={button}>
+                                    <a href={this.generateId(button)} className = "white-color link">
+                                        {button}
+                                        </a>
+                                </div>
+                                )
+                    })
+                }
             </div>
         </nav>
     )
